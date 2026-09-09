3b72ec27208eb4c2a098c343f04e53a006ebc72b
NikitaNevezhin <nevezhin.nikity@gmail.com>
2026-09-09 15:23:24 +0300

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -5,20 +5,28 @@
 
 #define SIZE 10
 
-void  PrintMatrix   (int* matrix, int length);
+void  PrintMatrix           (int* matrix, int length);
 
-void  SwapValues    (int* value1, int* value2);
+void  SwapValues            (int* value1, int* value2);
 
-int   DivideByEnd   (int *nums, int left, int right);
+int   DivideByEnd           (int *nums, int left, int right);
 
-void  QuickSort     (int* nums, int left, int right);
+void  QuickSort             (int* nums, int left, int right);
+
+int   ReverseDivideByEnd    (int *nums, int left, int right);
+
+void  ReverseQuickSort      (int* nums, int left, int right);
 
 
 int main(void)
 {
-    int nums[SIZE] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int nums[SIZE] = {10, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
-    QuickSort(nums, 0, SIZE);
+    QuickSort(nums, 0, SIZE - 1);
+
+    PrintMatrix(nums, SIZE);
+
+    ReverseQuickSort(nums, 0, SIZE - 1);
 
     PrintMatrix(nums, SIZE);
 
@@ -35,13 +43,21 @@ void PrintMatrix(int* matrix, int length)
 }
 
 
+void SwapValues(int* value1, int* value2)
+{
+    int temp = *value1;
+    *value1 = *value2;
+    *value2 = temp;
+}
+
+
 int DivideByEnd(int *nums, int left, int right)
 {
-    int main_elem = nums[right - 1];
+    int main_elem = nums[right];
 
     int less_nums_index = left;
 
-    for (int i = left; i < right; i++)
+    for (int i = left; i <= right; i++)
     {
         if (nums[i] <= main_elem)
         {
@@ -53,22 +69,44 @@ int DivideByEnd(int *nums, int left, int right)
     return less_nums_index - 1;
 }
 
-void QuickSort(int* nums, int left, int right)
+int ReverseDivideByEnd(int *nums, int left, int right)
 {
-    // printf("Executing QuickSort with left = %d, right = %d\n", left, right);
+    int main_elem = nums[right];
+
+    int less_nums_index = left;
+
+    for (int i = left; i <= right; i++)
+    {
+        if (nums[i] >= main_elem)
+        {
+            SwapValues(&nums[i], &nums[less_nums_index]);
+            less_nums_index++;
+        }
+    }
+    return less_nums_index - 1;
+}
+
+void QuickSort(int* nums, int left, int right)  // takes pointers on the part of the array "nums" that should be sorted
+{
+
     if (left < right)
     {
         int division = DivideByEnd(nums, left, right);
 
-        QuickSort(nums, left, division);
+        QuickSort(nums, left, division - 1);
+        QuickSort(nums, division + 1, right);
+    }
+}
+
+void ReverseQuickSort(int* nums, int left, int right)
+{
+    if (left < right)
+    {
+        int division = ReverseDivideByEnd(nums, left, right);
+
+        QuickSort(nums, left, division - 1);
         QuickSort(nums, division + 1, right);
     }
 }
 
 
-void SwapValues(int* value1, int* value2)
-{
-    int temp = *value1;
-    *value1 = *value2;
-    *value2 = temp;
-}
